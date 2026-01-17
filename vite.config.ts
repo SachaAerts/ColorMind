308ece0540ba92f859daf7f0a21eb210a5ff2042
SachaAerts <sacha50cl@hotmail.com>
2026-01-17 17:05:35 +0100

    add pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,45 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
       react(),
       tailwindcss(),
+      VitePWA({
+          registerType: 'autoUpdate',
+          includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+          manifest: {
+              name: 'Nom de votre application',
+              short_name: 'Nom court',
+              description: 'Description de votre app',
+              theme_color: '#ffffff',
+              background_color: '#ffffff',
+              display: 'standalone',
+              scope: '/',
+              start_url: '/',
+              icons: [
+                  {
+                      src: 'pwa-192x192.png',
+                      sizes: '192x192',
+                      type: 'image/png'
+                  },
+                  {
+                      src: 'pwa-512x512.png',
+                      sizes: '512x512',
+                      type: 'image/png'
+                  },
+                  {
+                      src: 'pwa-512x512.png',
+                      sizes: '512x512',
+                      type: 'image/png',
+                      purpose: 'any maskable'
+                  }
+              ]
+          }
+      })
   ],
     resolve: {
         alias: {
